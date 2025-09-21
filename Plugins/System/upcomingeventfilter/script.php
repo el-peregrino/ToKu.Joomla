181367a3f93017721f843d77adfd6d84a241abc9
el-peregrino <github@toku.cz>
2025-09-21 17:00:43 +0200

    upcoming event filter refactor

--- Plugins/System/upcomingeventfilter/script.php
+++ Plugins/System/upcomingeventfilter/script.php
@@ -20,7 +20,7 @@ return new class () implements InstallerScriptInterface {
 
     private string $minimumJoomla = '5.3.0';
     private string $minimumPhp = '8.2.0';
-    private string $minimumToKu = "1.0.3";
+    private string $minimumToKu = "1.0.10";
 
     public function install(InstallerAdapter $adapter): bool
     {
@@ -44,25 +44,25 @@ return new class () implements InstallerScriptInterface {
     {
         // php version
         if (version_compare(PHP_VERSION, $this->minimumPhp, '<')) {
-            Factory::getApplication()->enqueueMessage(sprintf(Text::_('JLIB_INSTALLER_MINIMUM_PHP'), $this->minimumPhp), 'error');
+            Factory::getApplication()->enqueueMessage(Text::sprintf('JLIB_INSTALLER_MINIMUM_PHP', $this->minimumPhp), 'error');
             return false;
         }
 
         // joomla version
         if (version_compare(JVERSION, $this->minimumJoomla, '<')) {
-            Factory::getApplication()->enqueueMessage(sprintf(Text::_('JLIB_INSTALLER_MINIMUM_JOOMLA'), $this->minimumJoomla), 'error');
+            Factory::getApplication()->enqueueMessage(Text::sprintf('JLIB_INSTALLER_MINIMUM_JOOMLA', $this->minimumJoomla), 'error');
             return false;
         }
 
         // dependency check
         if (!class_exists('ToKu\Library\JToKu')) {
-            Factory::getApplication()->enqueueMessage(sprintf("JToKu Library version %s is required.", $this->minimumToKu), 'error');
+            Factory::getApplication()->enqueueMessage(Text::sprintf('PLG_UPCOMINGEVENTFILTER_LIBRARY_ERROR', $this->minimumToKu), 'error');
             return false;
         }
 
         $version = \ToKu\Library\JToKu::VERSION ?? null;
         if (version_compare($version, $this->minimumToKu, '<')) {
-            Factory::getApplication()->enqueueMessage(sprintf("JToKu Library version %s is required.", $this->minimumToKu), 'error');
+            Factory::getApplication()->enqueueMessage(Text::sprintf('PLG_UPCOMINGEVENTFILTER_LIBRARY_ERROR', $this->minimumToKu), 'error');
             return false;
         }
 
@@ -92,8 +92,9 @@ return new class () implements InstallerScriptInterface {
         foreach ($files as $file) {
             try {
                 File::delete(JPATH_ROOT . $file);
-            } catch (\FilesystemException $e) { // global namespace
-                // \Joomla\Filesystem\Exception\FilesystemException
+            }
+            catch (\FilesystemException $e) { // global namespace
+                /** @var \Joomla\Filesystem\Exception\FilesystemException $e */
                 echo Text::sprintf('FILES_JOOMLA_ERROR_FILE_FOLDER', $file) . '<br>';
             }
         }
